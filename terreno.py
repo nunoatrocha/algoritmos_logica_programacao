f31af56870ff11e905c19acc8779f28d6dabdea9
Nuno Rocha <nuno.teles.rocha@gmail.com>
2023-04-20 04:50:34 +0100

    Adicionar terreno.py

---
+++ terreno.py
@@ -0,0 +1,11 @@
+largura = float(input("Digite a largura do terreno: "))
+comprimento = float(input("Digite o comprimento do terreno: "))
+valor = float(input("Digite do metro quadrado: "))
+
+area = largura * comprimento
+
+print(f"Area do terreno = {area:.2f}")
+
+preco = area * valor
+
+print(f"Area do terreno = {preco:.2f}")
